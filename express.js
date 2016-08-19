26f0a7725b5b02e882d7e8feb3ca95ff942e539a
Jordan Pectyo <NaCl4Ever@github.com>
2016-08-18 22:34:15 -0500

    Ride along base page reworked, now using REACT.JS basic nav bar set up and template set forth. Hello World! :)

---
+++ express.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var url = require('url');
+var app = express();
+	var ejs = require('ejs');
+app.listen(8080);
+app.set('views', './views/pages');
+app.set('view engine', 'ejs');
+// app.get('/find', function(req,res){
+// 	var url_parts = url.parse(req.url, true);
+// 	console.dir(url_parts);
+// });
+app.get('/home', function(req,res){
+	res.render('home');
+});
+app.get('/help', function(req,res){
+	res.render('help');
+	
+});
