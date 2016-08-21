86e13ef7efc0e82e40232465b4bd59580b821891
Jordan Pectyo <NaCl4Ever@github.com>
2016-08-20 22:27:01 -0500

    Component sourcing redone, navbar managed now account uses sidebare instead of redirecting to another page. Template for front page with cards prepared.

--- express.js
+++ express.js
@@ -5,6 +5,7 @@ var app = express();
 app.listen(8080);
 app.set('views', './views/pages');
 app.set('view engine', 'ejs');
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 // app.get('/find', function(req,res){
 // 	var url_parts = url.parse(req.url, true);
 // 	console.dir(url_parts);
